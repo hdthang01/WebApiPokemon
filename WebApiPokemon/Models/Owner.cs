ef0c555f74fc1f06924f681e9722936e140cacd3
hdthang01 <hdthang01@gmail.com>
2023-08-20 21:22:25 +0700

    Model initial

---
+++ WebApiPokemon/Models/Owner.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApiPokemon.Models
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gym { get; set; }
+    }
+}
